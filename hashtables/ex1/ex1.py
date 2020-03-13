bfc6deb231c16a414db8baad939b9ece1e2cb90a
zeleke <zzdd9251@gmail.com>
2020-03-13 09:30:09 -0700

    hashtable done...

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,8 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,6 +11,12 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+
+    for i in range(length):
+        j = hash_table_retrieve(ht, limit - weights[i])
+        if j is not None:
+            return (j, i) if j > i else (i, j)
+        hash_table_insert(ht, weights[i], i)
 
     return None
 
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,9 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        
+                        hash_table_retrieve
+                        )
 
 
 class Ticket:
@@ -20,4 +20,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
-    pass
+    for i in range(length):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+    
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    for i in range(1, length - 1):
+        route[i] = hash_table_retrieve(hashtable, route[i - 1])
+
+    route.pop()
+
+    return route
+
+    # pass
